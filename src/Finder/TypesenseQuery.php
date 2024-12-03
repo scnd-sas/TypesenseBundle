d5a2a0fa8e82618dc8832bcb7297185d5104faac
Yonel Ceruto <yonel.ceruto@scnd.com>
2024-12-03 00:11:58 -0500

    fixed php 8.4 deprecations

--- src/Finder/TypesenseQuery.php
+++ src/Finder/TypesenseQuery.php
@@ -8,7 +8,7 @@ class TypesenseQuery
 {
     private $searchParameters;
 
-    public function __construct(string $q = null, string $queryBy = null)
+    public function __construct(?string $q = null, ?string $queryBy = null)
     {
         $this->searchParameters = [];
         if ($q !== null) {
